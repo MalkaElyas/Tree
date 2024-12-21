4c288a6d74ba1ba577edc6b83cd45667f523f744
Malka Elyas <m0533178021@gmail.com>
2024-12-22 00:41:56 +0200

    loud and payment class

---
+++ Tree/class/Payment.cs
@@ -0,0 +1,22 @@
+﻿namespace Tree.@class
+{
+    public class Payment
+    {
+        private int id;
+        private DateOnly date;
+        private double principal;
+
+
+        #region constractors
+
+        #endregion
+
+        #region GET/SET
+
+        #endregion
+
+        #region ToString/Equals/CompareTo
+
+        #endregion
+    }
+}
